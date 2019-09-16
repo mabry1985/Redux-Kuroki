278744cc8593e4e046a412a910e2c9aaa4fdaf40
Student <example@example.com>
2019-09-16 11:39:28 -0700

    add dispatch and subscribe logic to app

--- scripts.js
+++ scripts.js
@@ -9,15 +9,20 @@ const initialState = {
 
 // REDUX REDUCER
 const reducer = (state = initialState, action) => {
+  let newState;
   switch (action.type) {
     case 'NEXT_LYRIC':
       let newArrayPosition = state.arrayPosition + 1;
-      let newState = {
+      newState = {
         songLyricsArray: state.songLyricsArray,
         arrayPosition: newArrayPosition,
       };
-
       return newState;
+
+    case 'RESTART_SONG':
+      newState = initialState;
+      return newState;
+
     default:
       return state;
   }
@@ -33,12 +38,48 @@ expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
   arrayPosition: 1,
 });
 
+expect(reducer({
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 1,
+},
+ { type: 'RESTART_SONG' })
+).toEqual(initialState);
+
 // REDUX STORE
 const { createStore } = Redux;
 const store = createStore(reducer);
 console.log(store.getState());
 
+// RENDERING STATE IN DOM
+const renderLyrics = () => {
+  // defines a lyricsDisplay constant referring to the div with a 'lyrics' ID in index.html
+  const lyricsDisplay = document.getElementById('lyrics');
+  // if there are already lyrics in this div, remove them one-by-one until it is empty:
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  // Locates the song lyric at the current arrayPosition:
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  // Creates DOM text node containing the song lyric identified in line above:
+  const renderedLine = document.createTextNode(currentLine);
+  // Adds text node created in line above to 'lyrics' div in DOM
+  document.getElementById('lyrics').appendChild(renderedLine);
+};
+
+// runs renderLyrics() method from above when page is finished loading.
+// window.onload is HTML5 version of jQuery's $(document).ready()
+window.onload = function () {
+  renderLyrics();
+};
+
 // CLICK LISTENER
 const userClick = () => {
-  console.log('click');
+  const currentState = store.getState();
+  if (currentState.arrayPosition === currentState.songLyricsArray.length -1) {
+    store.dispatch({ type: 'RESTART_SONG' });
+  } else {
+    store.dispatch({ type: 'NEXT_LYRIC' });
+  }
 };
+
+store.subscribe(renderLyrics);
